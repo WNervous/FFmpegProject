5f5a07fb84b9c6e1e1f41b1a30ae31023e7c6f38
WangYongSheng <m18301357350@163.com>
2019-08-05 19:38:37 +0800

    fixedddd

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,13 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include <libavformat/avformat.h>
+}
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_wys_ffmpegproject_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = av_version_info();
     return env->NewStringUTF(hello.c_str());
 }
